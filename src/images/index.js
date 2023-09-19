6fd2492781dc53503fc914fb0fa1a84acd9be194
AbuTalha3 <127955219+AbuTalha3@users.noreply.github.com>
2023-09-19 17:54:27 +0500

    commit all for once

---
+++ src/images/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import './styles/app.css';
+import reactLogo from './images/react.svg';
+
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+  <div>
+    <h1>Hello from React application</h1>
+    <img src={reactLogo} className="logo react" alt="React logo" />
+  </div>
+);
